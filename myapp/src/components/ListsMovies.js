82d1a85c5fa405d7ce00684577121430e96aec0e
mayssabensaid <mayoutam@gmail.com>
2022-08-25 12:30:10 +0100

    router

---
+++ myapp/src/components/ListsMovies.js
@@ -0,0 +1,48 @@
+export const ListsMovies =[{
+    id:Math.random(),
+    title:"Mademoiselle de Joncquières",
+    description:"Madame de La Pommeraye, jeune veuve retirée du monde, cède à la cour du marquis des Arcis, libertin notoire.",
+    posterUrl:"https://m.media-amazon.com/images/I/81MJ1uRCiMS._SL1500_.jpg",
+    Trailer:<iframe width="560" height="315" src="https://www.youtube.com/embed/g-7wCLIYOQY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    rate:5 ,
+
+},
+{
+id:Math.random(),
+title:"Avengers",
+description:"The Asgardian Loki encounters the Other, the leader of an extraterrestrial race known as the Chitauri.",
+posterUrl:"https://images-na.ssl-images-amazon.com/images/I/719SFBdxRtL._AC_SL1000_.jpg",
+Trailer:<iframe width="560" height="315" src="https://www.youtube.com/embed/lAbMtFUHPfc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+rate:3
+
+},
+
+{
+    id:Math.random(),
+    title:"Interceptor",
+    description:" Interceptor is a 2022 action drama film directed by Matthew Reilly from a screenplay that he co-wrote with Stuart Beattie. ",
+    posterUrl:"https://fr.web.img5.acsta.net/pictures/22/05/05/12/00/4437011.jpg",
+    Trailer:<iframe width="560" height="315" src="https://www.youtube.com/embed/OQSoII4Bj1o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    rate:2
+
+},
+
+{
+    id:Math.random(),
+    title:"Revenche",
+    description:"Revanche  centers on the ill-fated love story between a Viennese ex-con and a Ukrainian prostitute who get involved in a bank robbery.",
+    posterUrl:"https://i.ytimg.com/vi/XPs7XWNOfsc/maxresdefault.jpg",
+    Trailer:<iframe width="560" height="315" src="https://www.youtube.com/embed/XPs7XWNOfsc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    rate:4
+
+},
+{
+    id:Math.random(),
+    title:"Split",
+    description:" kidnaps three teenagers. They must figure out his friendly personas before he unleashes his 24th personality.",
+    posterUrl:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSkXKp2opNa6U_LLo07y_3d_zxZ0xA80OmrdXXOO4bQkkB1Asnc",
+    Trailer:<iframe width="560" height="315" src="https://www.youtube.com/embed/kQlOmLhrELo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    rate:4
+
+},
+]
